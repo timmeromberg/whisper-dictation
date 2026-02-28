f7f0679f85eb272a146137b07c9d35a4c9f90cc0
timme <timmeromberg@gmail.com>
2026-02-28 10:54:40 +0100

    feat: context-sensitive auto-send (terminals/IDEs only)
    Detect frontmost app via NSWorkspace and only press Enter after
    paste when the active app is a terminal or IDE.

--- paster.py
+++ paster.py
@@ -7,10 +7,34 @@ import time
 
 import pyperclip
 import Quartz
+from AppKit import NSWorkspace
 from pynput.keyboard import Controller, Key
 
 # macOS virtual key code for Return
 _VK_RETURN = 36
+
+# Bundle IDs of apps where auto_send should fire
+_TERMINAL_BUNDLES = {
+    "com.apple.Terminal",
+    "com.googlecode.iterm2",
+    "com.jetbrains.intellij",
+    "com.jetbrains.intellij.ce",
+    "com.jetbrains.pycharm",
+    "com.jetbrains.pycharm.ce",
+    "com.jetbrains.WebStorm",
+    "com.jetbrains.CLion",
+    "com.jetbrains.goland",
+    "com.jetbrains.rider",
+    "com.jetbrains.rubymine",
+    "com.jetbrains.PhpStorm",
+    "com.jetbrains.datagrip",
+    "net.kovidgoyal.kitty",
+    "io.alacritty",
+    "com.github.wez.wezterm",
+    "dev.warp.Warp-Stable",
+    "co.zeit.hyper",
+    "com.microsoft.VSCode",
+}
 
 
 def _post_key(vk: int) -> None:
@@ -19,6 +43,15 @@ def _post_key(vk: int) -> None:
     Quartz.CGEventPost(Quartz.kCGHIDEventTap, down)
     up = Quartz.CGEventCreateKeyboardEvent(None, vk, False)
     Quartz.CGEventPost(Quartz.kCGHIDEventTap, up)
+
+
+def _frontmost_is_terminal() -> bool:
+    """Check if the frontmost app is a terminal/IDE."""
+    app = NSWorkspace.sharedWorkspace().frontmostApplication()
+    if app is None:
+        return False
+    bundle_id = app.bundleIdentifier() or ""
+    return bundle_id in _TERMINAL_BUNDLES
 
 
 class TextPaster:
@@ -42,6 +75,6 @@ class TextPaster:
                 self._keyboard.press("v")
                 self._keyboard.release("v")
 
-            if self.auto_send:
+            if self.auto_send and _frontmost_is_terminal():
                 time.sleep(0.3)
                 _post_key(_VK_RETURN)
